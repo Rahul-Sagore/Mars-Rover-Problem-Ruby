2cce75ae5d5928369691eaeaafe795e2dc9b37b5
Rahul-Sagore <rahul.sagore@gmail.com>
2016-04-28 19:29:21 +0530

    Updated code for taking sample input from file

--- mars_rover_ruby_methods.rb
+++ mars_rover_ruby_methods.rb
@@ -1,19 +1,48 @@
+def input_to_array(is_word=nil) #GENERAL CLASS FOR TAKING INPUT AND CONVERTING TO ARRAY
+	user_input = gets
+	final_result = nil
+	if(is_word)
+		final_result = user_input.split("")
+		final_result.delete_at(-1) if final_result[-1] == "\n"
+	else
+		if !user_input.nil?
+			user_input = user_input.split(" ")
+			final_result = user_input.each{|x| x }
+		end
+	end
+	final_result
+end
+
 class MarsMission # MAIN CLASS TO START THE MISSION
 	DIRECTIONS = ["N", "E", "S", "W"]
 
-	def initialize(x_upper, y_upper)
-		@plateau = [x_upper, y_upper]
+	def initialize()
+		@rovers = []
 	end
 
 	def init_rovers(x, y, head, controls) #INIT ROVER INFO IN HASH
-		return { x: x, y: y, direction: head, controls: controls.split("") }
+		return { x: x.to_i, y: y.to_i, direction: head, controls: controls }
+	end
+
+	def start
+		@plateau = input_to_array()	
+		while true
+			pos = input_to_array()
+			break if pos == [] || pos.nil?
+			controls = input_to_array(true)
+			pos.push controls
+			@rovers.push(init_rovers(pos[0], pos[1], pos[2], pos[3]))
+		end
+		@rovers.each { |rover|
+			control_rover(rover)
+		}
 	end
 
 	def control_rover(rover) #Direct the rover based on control
 		rover[:controls].each { |status|
 			update_position(rover, status)
 		}
-		return "#{rover[:x]}, #{rover[:y]}, #{rover[:direction]}"
+		puts "#{rover[:x]}, #{rover[:y]}, #{rover[:direction]}"
 	end
 
 	def update_position(rover, current_control) # UPDATE ROVER's POSITION BASED ON STRING SENT BY NASA :D
@@ -45,10 +74,13 @@ class MarsMission # MAIN CLASS TO START THE MISSION
 			when "W" then rover[:x] -= 1
 		end
 
-		x_crossed = rover[:x] > @plateau[0] || rover[:x] < 0
-		y_crossed = rover[:y] > @plateau[1] || rover[:y] < 0 
+		x_crossed = rover[:x] > @plateau[0].to_i || rover[:x] < 0
+		y_crossed = rover[:y] > @plateau[1].to_i || rover[:y] < 0 
 		raise "Rover moved out of the plane plateu" if (x_crossed || y_crossed)
 
 		return "#{rover[:x]}, #{rover[:y]}"
 	end
 end
+
+mission = MarsMission.new()
+mission.start
